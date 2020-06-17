610481336390bc86053bc53a758e9a15533efc62
Aleksey Frolov <wwwalexfrolov@mail.ru>
2020-06-17 18:14:23 +0300

    commit before big changes

--- app/controllers/web/password_resets_controller.rb
+++ app/controllers/web/password_resets_controller.rb
@@ -4,10 +4,14 @@ class Web::PasswordResetsController < Web::ApplicationController
   end
 
   def create
+    @password_reset = PasswordResetForm.new(password_reset_params)
 
     user.send_password_reset if user
     flash[:notice] = 'E-mail sent with password reset instructions.'
     redirect_to(:new_session)
   end
-
+  
+  def password_reset_params
+    params.require(:password_reset_form).permit(:email)
+  end
 end
