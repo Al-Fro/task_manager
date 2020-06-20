43879a7c29d205bfead93ba45fecb9aaeac9bb48
Aleksey Frolov <wwwalexfrolov@mail.ru>
2020-06-20 09:46:15 +0300

    delete validation password

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true, length: { minimum: 2 }
   validates :last_name, presence: true, length: { minimum: 2 }
   validates :email, presence: true, format: { with: /@/ }, uniqueness: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
 
   def generate_password_token!
     self.password_reset_token = generate_token
